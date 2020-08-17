5fa907d85c201b49fea961f56eb0c46f15434996
Daniel Pineda <dpinedaj@unal.edu.co>
2020-08-17 11:11:14 -0500

    Change mongo db to use Atlas db

--- back_app/app.py
+++ back_app/app.py
@@ -4,16 +4,34 @@ from flask_mongoengine import MongoEngine
 from api.test.test_routes import test_routes
 from flask_cors import CORS
 
-config = {'MONGODB_SETTINGS': {
-    'db': 'dev',
-    'host': 'localhost',
-    'port': 27017,
-    'username': 'admin',
-    'password': 'admin',
-    'authentication_source': 'admin'}}
+#config = {'MONGODB_SETTINGS': {
+#    'db': 'dev',
+#    'host': 'localhost',
+#    'port': 27017,
+#    'username': 'admin',
+#    'password': 'admin',
+#    'authentication_source': 'admin'}}
+
+
+##BASE DE DATOS DE ATLAS
+DEFAULT_CONFIG = {
+    "database": {
+        "user": "admin",
+        "pwd": "admin",
+        "clusterName": "cluster0",
+        "database": "test",
+    }
+}
+
+ATLAS_URI = "mongodb+srv://{user}:{pwd}@{clusterName}.ytibs.mongodb.net/{database}?retryWrites=true&w=majority"
+DB_URI = ATLAS_URI.format(**DEFAULT_CONFIG["database"])
+##############################################################
+
+
 app = Flask(__name__)
 CORS(app)
-app.config.update(config)
+app.config["MONGODB_HOST"] = DB_URI #BD ATLAS
+#app.config.update(config)
 db = MongoEngine(app=app)
 api = Api(app=app)
 
