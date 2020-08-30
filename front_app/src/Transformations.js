17d053d1d722efd6ea8d14fb74e3884fcb0c8767
Jontal7 <jontal7@gmail.com>
2020-08-29 22:00:34 -0500

    probando navbar

---
+++ front_app/src/Transformations.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  margin-top: 1em;
+  margin-left: 6em;
+  margin-right: 6em;
+`;
+export const Transformations = () => (
+  <Wrapper>
+    <h2>Transformations</h2>
+  </Wrapper>
+)
--- front_app/src/containers/App.js
+++ front_app/src/containers/App.js
@@ -2,16 +2,24 @@ import React from 'react';
 import '../App.css';
 import logo from '../logo.svg';
 import TestContainer from './TestContainer'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { NavigationBar } from '../components/NavigationBar';
+import { Home } from '../Home';
+import { Data } from '../Data';
+import { Transformations } from '../Transformations';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <TestContainer />
-      </header>
-    </div>
+    <React.Fragment>
+      <Router>
+        <NavigationBar />
+        <React.Component/>
+      </Router>
+    </React.Fragment>
   );
 }
+
 
 export default App;
